c52461275d27a55cf1f492c6bea67415e4449eab
Tomoms <fondatommaso2@gmail.com>
2018-01-08 20:19:22 +0100

    Fix the Mobile Radadio Active bug
    Change-Id: I1097519addca790beb75f9785787539baa6a7e33

--- services/core/java/com/android/server/NetworkManagementService.java
+++ services/core/java/com/android/server/NetworkManagementService.java
@@ -928,7 +928,7 @@ public class NetworkManagementService extends INetworkManagementService.Stub
                     notifyInterfaceClassActivity(Integer.parseInt(cooked[3]),
                             isActive ? DataConnectionRealTimeInfo.DC_POWER_STATE_HIGH
                             : DataConnectionRealTimeInfo.DC_POWER_STATE_LOW,
-                            timestampNanos, processUid, false);
+                            timestampNanos, processUid, true);
                     return true;
                     // break;
             case NetdResponseCode.InterfaceAddressChange:
@@ -1745,7 +1745,7 @@ public class NetworkManagementService extends INetworkManagementService.Stub
                 @Override public void run() {
                     notifyInterfaceClassActivity(type,
                             DataConnectionRealTimeInfo.DC_POWER_STATE_HIGH,
-                            SystemClock.elapsedRealtimeNanos(), -1, false);
+                            SystemClock.elapsedRealtimeNanos(), -1, true);
                 }
             });
         }
@@ -1774,7 +1774,7 @@ public class NetworkManagementService extends INetworkManagementService.Stub
                 @Override public void run() {
                     notifyInterfaceClassActivity(params.type,
                             DataConnectionRealTimeInfo.DC_POWER_STATE_LOW,
-                            SystemClock.elapsedRealtimeNanos(), -1, false);
+                            SystemClock.elapsedRealtimeNanos(), -1, true);
                 }
             });
         }
