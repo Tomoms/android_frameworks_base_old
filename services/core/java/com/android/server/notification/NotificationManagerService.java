5754fd37b126ceb01959a03398ee08f9c79c93da
Seungho Lee <shiny.lee@samsung.com>
2018-12-09 19:27:36 +0900

    Update sound/vibrate notification key only when it actually buzz/beep
    Test: Make notification with vibration pattern with [0]
    Test: Check mVibrateNotificationKey unchanged
    Change-Id: I208797f370ac5a30db5fcc316be956335a418319
    Merged-In: I208797f370ac5a30db5fcc316be956335a418319
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -4932,12 +4932,14 @@ public class NotificationManagerService extends SystemService {
                     }
                     if (DBG) Slog.v(TAG, "Interrupting!");
                     if (hasValidSound) {
-                        mSoundNotificationKey = key;
                         if (mInCall) {
                             playInCallNotification();
                             beep = true;
                         } else {
                             beep = playSound(record, soundUri);
+                        }
+                        if(beep) {
+                            mSoundNotificationKey = key;
                         }
                     }
 
@@ -4945,9 +4947,10 @@ public class NotificationManagerService extends SystemService {
                             mAudioManager.getRingerModeInternal()
                                     == AudioManager.RINGER_MODE_SILENT;
                     if (!mInCall && hasValidVibrate && !ringerModeSilent) {
-                        mVibrateNotificationKey = key;
-
                         buzz = playVibration(record, vibration, hasValidSound);
+                        if(buzz) {
+                            mVibrateNotificationKey = key;
+                        }
                     }
                 }
             }
