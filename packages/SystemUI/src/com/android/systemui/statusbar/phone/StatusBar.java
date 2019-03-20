10a68af950b10fb83c914ac009eef97da4eb1116
dkati <dkatikaridis@gmail.com>
2019-03-20 21:07:31 +0200

    SystemUI: Stop StatusBar logcat spam
    Change-Id: I11593127e049235ce5151660f7c9f0a467c6a0d6

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1930,6 +1930,7 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
                 old2, state2, diff2));
         }
 
+	/*
         StringBuilder flagdbg = new StringBuilder();
         flagdbg.append("disable<");
         flagdbg.append(0 != ((state1 & StatusBarManager.DISABLE_EXPAND))                ? 'E' : 'e');
@@ -1959,7 +1960,8 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
         flagdbg.append(0 != ((diff2  & StatusBarManager.DISABLE2_NOTIFICATION_SHADE))   ? '!' : ' ');
         flagdbg.append('>');
         Log.d(TAG, flagdbg.toString());
-
+	*/
+	
         if ((diff1 & StatusBarManager.DISABLE_EXPAND) != 0) {
             if ((state1 & StatusBarManager.DISABLE_EXPAND) != 0) {
                 animateCollapsePanels();
