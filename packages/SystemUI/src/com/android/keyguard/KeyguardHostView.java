c8784c3d7efc930dbca28c7a023219ade6c23a0b
Tomoms <fondatommaso2@gmail.com>
2019-11-30 10:40:51 +0100

    Revert "Add preference for automatic face unlocking [1/2]"
    This reverts commit c02ee623b77f2b8aa6880f65d6945c62391d1e10.

--- packages/SystemUI/src/com/android/keyguard/KeyguardHostView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardHostView.java
@@ -18,14 +18,11 @@ package com.android.keyguard;
 
 import android.app.Activity;
 import android.app.ActivityManager;
-import android.content.ContentResolver;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Canvas;
 import android.media.AudioManager;
-import android.provider.Settings;
 import android.os.SystemClock;
-import android.os.UserHandle;
 import android.service.trust.TrustAgentService;
 import android.telephony.TelephonyManager;
 import android.util.AttributeSet;
@@ -104,10 +101,7 @@ public class KeyguardHostView extends FrameLayout implements SecurityCallback {
         @Override
         public void onTrustChanged(int userId) {
             if (userId != KeyguardUpdateMonitor.getCurrentUser()) return;
-            int mFaceAuto = Settings.Secure.getIntForUser(getContext().getContentResolver(),
-                                  Settings.Secure.FACE_AUTO_UNLOCK, 0, UserHandle.USER_CURRENT);
-            if (mKeyguardUpdateMonitor.getUserCanSkipBouncer(userId)
-                            && mKeyguardUpdateMonitor.getUserHasTrust(userId) && mFaceAuto == 1) {
+            if (mKeyguardUpdateMonitor.getUserCanSkipBouncer(userId) && mKeyguardUpdateMonitor.getUserHasTrust(userId)){
                 dismiss(false, userId);
             }
         }
